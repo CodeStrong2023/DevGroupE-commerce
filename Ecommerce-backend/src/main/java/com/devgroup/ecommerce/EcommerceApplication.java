6a8f0795dd8ef9cd2fab021ae27de8c0407b68c9
Angelo Vellar <angelovellar24@gmail.com>
2024-09-15 21:40:34 -0300

    Update:Agregando back y mod.carpetas2

--- Ecommerce-backend/src/main/java/com/devgroup/ecommerce/EcommerceApplication.java
+++ Ecommerce-backend/src/main/java/com/devgroup/ecommerce/EcommerceApplication.java
@@ -1,4 +1,4 @@
-package com.devgroup.ecomerce;
+package com.devgroup.ecommerce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
