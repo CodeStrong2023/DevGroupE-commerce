de7196ffb5ef3556275c5fe7cb6ee67d078ee53b
Angelo Vellar <angelovellar24@gmail.com>
2024-09-16 22:03:55 -0300

    Update: Agregando Mod. a Clase Game

--- Ecommerce-backend/src/main/java/com/devgroup/ecommerce/controllers/GamesController.java
+++ Ecommerce-backend/src/main/java/com/devgroup/ecommerce/controllers/GamesController.java
@@ -1,9 +1,24 @@
 package com.devgroup.ecommerce.controllers;
 
+import com.devgroup.ecommerce.dto.GameDTO;
+import com.devgroup.ecommerce.service.GameService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
+    // LA CLASE GameController expone los endpoints para interactuar con los juegos.
 @RestController
 @RequestMapping("/games")
 public class GamesController {
+    @Autowired
+    private GameService gameService;
+
+    @GetMapping
+    public List<GameDTO> getAllGames(){
+        return gameService.getAllGames();
+    }
+
 }
--- Ecommerce-backend/src/main/java/com/devgroup/ecommerce/repository/GameRepository.java
+++ Ecommerce-backend/src/main/java/com/devgroup/ecommerce/repository/GameRepository.java
@@ -3,5 +3,6 @@ package com.devgroup.ecommerce.repository;
 import com.devgroup.ecommerce.models.Game;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+    // LA CLASE GameRepository interactúa con la base de datos y utiliza la entidad Game.
 public interface GameRepository extends JpaRepository<Game,Long> {
 }
