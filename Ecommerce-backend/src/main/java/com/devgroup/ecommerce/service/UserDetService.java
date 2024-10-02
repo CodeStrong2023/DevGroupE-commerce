50802df3515bfc1721034b277a0e5862fd8cd1ab
Joaquin Esposito <joaquiesposito@gmail.com>
2024-10-02 20:19:07 -0300

    User arreglado?

--- Ecommerce-backend/src/main/java/com/devgroup/ecommerce/service/UserDetService.java
+++ Ecommerce-backend/src/main/java/com/devgroup/ecommerce/service/UserDetService.java
@@ -1,8 +1,8 @@
 package com.devgroup.ecommerce.service;
 
+import com.devgroup.ecommerce.models.User;
 import com.devgroup.ecommerce.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -26,12 +26,7 @@ public class UserDetService implements UserDetailsService {
             throw new UsernameNotFoundException("Usuario no encontrado con el nombre de usuario: " + username);
         }
 
-        // Convertir User a un objeto que implemente UserDetails
-        User user = userOptional.get();
-        return org.springframework.security.core.userdetails.User.builder()
-                .username(user.getUsername())
-                .password(user.getPassword())  // La contraseña ya debe estar cifrada en la base de datos
-                .roles("USER")  // Aquí puedes asignar roles; en este ejemplo se asigna un rol fijo
-                .build();
+        // Retornar el objeto User, que ya implementa UserDetails
+        return userOptional.get();
     }
 }
